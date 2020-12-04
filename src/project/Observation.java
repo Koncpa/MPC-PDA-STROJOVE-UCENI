7e6fac97830aaae294cfa0068fa5c8ab358c02d5
ARTeriosclerosis <liborsas@gmail.com>
2020-12-04 14:20:59 +0100

    Normalization update 2

--- src/project/Observation.java
+++ src/project/Observation.java
@@ -5,20 +5,21 @@ import java.util.Locale;
 import robocode.Bullet;
 
 class Observation {
-    private double bearing;
-    private double heading;
     private double distance;
     private double velocity;
-    private double robotHeading;
     private Bullet bullet;
-
+    private double relativeHeading;
+    private double relativeBulletHeading;
+    private double gunToTurn;
+    
     Observation(double bearing, double heading, double distance, double velocity, double robotHeading, Bullet bullet) {
-        this.bearing = bearing;
-        this.heading = heading;
         this.distance = distance;
         this.velocity = velocity;
-        this.robotHeading = robotHeading;
         this.bullet = bullet;
+        
+        this.relativeHeading = toRelative(heading - robotHeading);
+        this.relativeBulletHeading = toRelative(bullet.getHeading() - robotHeading);
+        this.gunToTurn = toRelative(bearing - relativeBulletHeading);
     }
 
     @Override
@@ -26,10 +27,6 @@ class Observation {
         // if bullet is still active don't use this observation
         if (bullet.isActive())
             return "";
-        
-        double relativeHeading = toRelative(heading - robotHeading);
-        double relativeBulletHeading = toRelative(bullet.getHeading() - robotHeading);
-        double gunToTurn = toRelative(bearing - relativeBulletHeading);
         
         return String.format(Locale.US, "%.2f;%.2f;%.2f;%.2f;%d\n", gunToTurn, relativeHeading, distance, velocity, (bullet.getVictim() == null) ? 0 : 1);
     }
