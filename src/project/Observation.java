042fd05c997e5b2d96c5621a5849016e6881fe6f
ARTeriosclerosis <liborsas@gmail.com>
2020-12-04 14:08:38 +0100

    Normalization update

--- src/project/Observation.java
+++ src/project/Observation.java
@@ -9,13 +9,15 @@ class Observation {
     private double heading;
     private double distance;
     private double velocity;
+    private double robotHeading;
     private Bullet bullet;
 
-    Observation(double bearing, double heading, double distance, double velocity, Bullet bullet) {
+    Observation(double bearing, double heading, double distance, double velocity, double robotHeading, Bullet bullet) {
         this.bearing = bearing;
         this.heading = heading;
         this.distance = distance;
         this.velocity = velocity;
+        this.robotHeading = robotHeading;
         this.bullet = bullet;
     }
 
@@ -24,6 +26,15 @@ class Observation {
         // if bullet is still active don't use this observation
         if (bullet.isActive())
             return "";
-        return String.format(Locale.US, "%.2f;%.2f;%.2f;%.2f;%.2f;%d\n", bearing, heading, distance, velocity, bullet.getHeading(), (bullet.getVictim() == null) ? 0 : 1);
+        
+        double relativeHeading = toRelative(heading - robotHeading);
+        double relativeBulletHeading = toRelative(bullet.getHeading() - robotHeading);
+        double gunToTurn = toRelative(bearing - relativeBulletHeading);
+        
+        return String.format(Locale.US, "%.2f;%.2f;%.2f;%.2f;%d\n", gunToTurn, relativeHeading, distance, velocity, (bullet.getVictim() == null) ? 0 : 1);
+    }
+    
+    private double toRelative(double input) {
+    	return (input >= 180 ) ? -(360 - input) : (input < -180) ? (360 + input) : input;
     }
 }
